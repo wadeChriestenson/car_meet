c40450867cf30ff92c7b42cbc0588b7b07a03b3e
Wade Chriestenson <wadechriestenson@gmail.com>
2022-04-04 06:31:13 -0700

    04/04/2022 - WC

--- salem/urls.py
+++ salem/urls.py
@@ -1,5 +1,6 @@
 from django.urls import path
 from salem import views
+from django.views.generic.base import TemplateView #import TemplateView
 
 urlpatterns = [
     path('', views.carMeet, name='carMeet'),
@@ -7,4 +8,5 @@ urlpatterns = [
     path('aboutus.html', views.aboutus, name='aboutus'),
     path('disclaimer.html', views.disclaimer, name='disclaimer'),
     path('redirect.html', views.dataInput, name='redirect')
+    path("robots.txt",TemplateView.as_view(template_name="templates/robots.txt", content_type="text/plain")),  #add the robots.txt file
 ]
