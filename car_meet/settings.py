d61e995f69ccf718651cc898eec06859cef87998
Wade Chriestenson <wadechriestenson@gmail.com>
2022-04-14 13:56:17 -0700

    Update settings.py

--- car_meet/settings.py
+++ car_meet/settings.py
@@ -24,7 +24,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = os.environ('SECRET_KEY')
+SECRET_KEY = 'django-insecure-^)9nestou=7^#+ehcll22&387qi#si%y)3zun4ut(@i02$s1-q'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
