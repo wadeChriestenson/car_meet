f0e0ee30f39c5bc2cae448b94443e26fe3032b2c
Wade Chriestenson <wadechriestenson@gmail.com>
2022-04-14 12:40:54 -0700

    04/14/2022 - WC

--- car_meet/settings.py
+++ car_meet/settings.py
@@ -12,6 +12,9 @@ https://docs.djangoproject.com/en/4.0/ref/settings/
 
 from pathlib import Path
 import django_heroku
+import os
+
+
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -21,10 +24,10 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-^)9nestou=7^#+ehcll22&387qi#si%y)3zun4ut(@i02$s1-q'
+SECRET_KEY = os.getenv('SECRET_KEY', 'Value')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = []
 
